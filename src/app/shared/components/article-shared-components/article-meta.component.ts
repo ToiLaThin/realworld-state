b1f5bea9482cbd67f1f36a7d7821c167b27c2df8
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-01-30 00:58:13 +0700

    switch to new control flow & change detection

--- src/app/shared/components/article-shared-components/article-meta.component.ts
+++ src/app/shared/components/article-shared-components/article-meta.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { IArticle } from "../../../core/models/article.interface";
 @Component({
     selector: 'rw-article-meta',
     templateUrl: './article-meta.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ArticleMetaComponent {
     @Input() article!: IArticle;
