c0b90df5c151a222bc4b007b26f182920cf4787c
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-02-13 17:04:26 +0700

    add docker to containerize backend, settings remote cloud azure postgres db, config to connected to deploy app service azure, change fe base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    API_BASE_URL: 'https://api.realworld.io/api',
+    //API_BASE_URL: 'https://api.realworld.io/api',    
+    // API_BASE_URL: 'https://localhost:32770/api' // Docker (the port can change)
+    API_BASE_URL: 'https://realworldapiapp.azurewebsites.net/api' // Deploy to Azure Service app
 };
