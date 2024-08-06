92c354e3bb8b1bbb0b03d5e5ec33e736b889c135
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-06 13:14:47 +0700

    + redeploy BE to azure container registry and have new url, change
    connStr to DB
    + change FE env to BE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     //API_BASE_URL: 'https://api.realworld.io/api',    
     // API_BASE_URL: 'https://localhost:32770/api' // Docker (the port can change)
-    API_BASE_URL: 'https://realworldapiapp.azurewebsites.net/api' // Deploy to Azure Service app
+    API_BASE_URL: 'https://realworldapiapp-dabnfda0gqadasew.southeastasia-01.azurewebsites.net/api' // Deploy to Azure Service app
 };
