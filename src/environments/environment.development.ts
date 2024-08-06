6e06460400909bec63903059b91736760a79d4dd
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-06 13:24:18 +0700

    change env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_BASE_URL: 'http://localhost:5159/api', // Local
+    // API_BASE_URL: 'http://localhost:5159/api', // Local
     // API_BASE_URL: 'https://localhost:32770/api' // Docker (the port can change)
-    // API_BASE_URL: 'https://realworldapiapp-dabnfda0gqadasew.southeastasia-01.azurewebsites.net/api' // Deploy to Azure Service app
+    API_BASE_URL: 'https://realworldapiapp-dabnfda0gqadasew.southeastasia-01.azurewebsites.net/api' // Deploy to Azure Service app
 };
